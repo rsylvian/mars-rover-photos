dc7664997b6d3f15f7dc59cc2f8229661ff277d2
MyBoon <romain.sylvian@gmail.com>
2018-12-05 22:57:53 -0800

    add catch-all route

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import { Route, Link, Redirect } from 'react-router-dom';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Home from '../Home';
 
 import { RoversType } from '../../services/MarsPhotosAPI';
 
 import { Menu, Icon } from 'antd';
 
-const App = () => (
-  <div>
-    <Menu mode="horizontal">
-      {Object.values(RoversType).map(type => {
-        return (
-          <Menu.Item key={type}>
-            <Link to={'/' + type}>
-              <Icon type="camera" />
-              {type}
-            </Link>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
-    <main>
-      <Route exact path="/:rover" component={Home} />
-      {/* <Redirect from="/" to="/curiosity" /> */}
-    </main>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    // get all the type of rovers to create a route with regex validation
+    // will generate something like /:rover(a|b|c)
+    const allowedParameters = Object.values(RoversType);
 
-export default App;
+    return (
+      <div>
+        <Menu
+          selectedKeys={[this.props.currentPath.substr(1)]}
+          mode="horizontal"
+        >
+          {Object.values(RoversType).map(type => {
+            return (
+              <Menu.Item key={type}>
+                <Link to={'/' + type}>
+                  <Icon type="camera" />
+                  {type}
+                </Link>
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+        <main>
+          <Switch>
+            {/* route for rover page */}
+            <Route
+              exact
+              path={'/:rover(' + allowedParameters.join('|') + ')'}
+              component={Home}
+            />
+
+            {/* catch all with redirect to default rover (curiosity) */}
+            <Route render={() => <Redirect to="/curiosity" />} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ router }) => ({
+  currentPath: router.location.pathname
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(App);
